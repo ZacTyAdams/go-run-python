f6900195f66652a567b36ee8a3290bd8fad08f19
Zac Adams <zac@zacthe.dev>
2026-02-05 21:16:53 -0600

    adjust embedding

--- python_embed_android_arm64.go
+++ python_embed_android_arm64.go
@@ -4,6 +4,6 @@ package gorunpython
 
 import _ "embed"
 
-//go:embed universal-bucket/linux-arm64.tar.gz
+//go:embed universal-bucket/android-arm64.tar.gz
 var embeddedPython []byte
 var PythonVersion = "3.15"
